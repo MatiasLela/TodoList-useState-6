02c325adddc993207be47c98751d552b3b5641cf
MatiasLela <matiaslela0@gmail.com>
2023-04-06 00:28:30 -0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  function handleInputChange(e){
+    e.preventDefault();
+    setInput(e.target.value);
+  };
+
+  function handleAddTodo(){
+    if (input.trim() !== "") {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
+
+  function handleRemoveTodo(index){
+    const nuevoTodos = [...todos];
+    nuevoTodos.splice(index, 1);
+    setTodos(nuevoTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de tareas pendientes</h1>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleAddTodo}>Agregar</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleRemoveTodo(index)}>Eliminar</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
